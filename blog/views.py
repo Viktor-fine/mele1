d9069435fd2e78d8d1692a6f208628c3be105902
Viktor-fine <vik-mailll@mail.ru>
2025-07-30 01:40:37 +0300

    отображена первая страница тем

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,9 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
-    post = get_list_or_404(Post, slug=post, status='published', publish__year=year, publish_month=month, publish__day=day)
+    post = get_list_or_404(Post, slug=post,
+                            status='published',
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day)
     return render(request, 'blog/post/detail.html', {'post':post})
